b2b250e41083e31a0eb9da8feabb5cc5b436da0a
PsarevDenis <31490990+PsarevDenis@users.noreply.github.com>
2017-08-31 00:35:50 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using DevExpress.Mvvm.DataAnnotations;
+
+namespace LadleBubble
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DevExpress.Xpf.Core.DXGridDataController.DisableThreadingProblemsDetection = true;
+        }
+    }
+}
